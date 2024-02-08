f58ba6a3693ba456794458fcf7e7583a2a4bc129
Keith <haiimkeith@gmail.com>
2024-02-07 23:53:31 -0800

    Initial commit

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,54 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+
+# test_counter.py
+
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app, COUNTERS
+
+# we need to import the file that contains the status codes
+from src import status 
+
+
+class CounterTest(TestCase):
+  """Counter tests"""
+
+  def test_create_a_counter(self):
+    """It should create a counter"""
+    client = app.test_client()
+    result = client.post('/counters/foo')
+    self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+  
+  
+  def setUp(self):
+    self.client = app.test_client()
+
+  def test_duplicate_a_counter(self):
+    """It should return an error for duplicates"""
+    result = self.client.post('/counters/bar')
+    self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+    result = self.client.post('/counters/bar')
+    self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+
+  def test_update_a_counter(self):
+    result = self.client.post('/counters/abc')
+    self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+    self.assertEqual(result.json['abc'], 0)
+
+    result = self.client.put('/counters/abc')
+    self.assertEqual(result.status_code, status.HTTP_200_OK)
+    self.assertEqual(result.json['abc'], 1)
+
+  def test_get_a_counter(self):
+    result = self.client.post('/counters/kdr')
+    self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+    result = self.client.get('/counters/kdr')
+    self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+    result = self.client.get('/counters/doesntexist')
+    self.assertEqual(result.status_code, status.HTTP_409_CONFLICT) 
